49c29aae41c091caec9aa8b6f72ab69648a82a1e
Patricia Cordero <patricia@patriciaco.com>
2020-04-22 17:17:12 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,15 @@
-def reverse_each_word(string)
-    sentence = string.split(" ")
-    second_array = []
-    sentence.each { |word| second_array << word.reverse}
-  second_array.join(" ")
-end
 
 
 def reverse_each_word(string)
   sentence = string.split(" ").collect { |word| word.reverse }
   sentence.join(" ")
 end
+
+# string = []
+# def reverse_each_word(string)
+#     string.each {|word| string << word.reverse}
+#   end
+#   string.join(" ")
+# end
+
+  
