16b37d2dba47aab6c00c9741b51c7e2ed897c560
Joann_Brady <jbrady11934@optimum.net>
2020-04-22 00:27:33 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+
+
+ def reverse_each_word(string)
+     reverse_sentence= []
+     string.split.collect do |string|
+         reverse_sentence << string.reverse
+     end 
+     reverse_sentence.join(" ")
+ end
+
+
