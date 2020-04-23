601fbf5737f6ba1630b94c56fc970cf1e39daa98
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-23 19:47:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,9 @@ def reverse_each_word(sentence)
   # in my sentence array I want you to put a space between each word (.split)
   new_array = []
   # this is where I am storing that reversed array
-  new_array = words_array.collect {|word| word.reverse }
+  words_array.collect {|word| word.reverse }
         # I want my array to put a space in between each word and reverse it and move it over to the "new array"
   new_array.join(" ")
       # we are joining the new array into one big old family aka. one string
 end
+  # new_array =
