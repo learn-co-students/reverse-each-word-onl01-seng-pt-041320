8d2b0109708aa0674c9963603326c4f31944d4c2
Lamont30265 <L.harris008@icloud.com>
2020-05-27 21:42:03 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(array)
+  array.split(" ").each {|x| x.reverse!}.join(" ")
+end
+    
+
+
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
